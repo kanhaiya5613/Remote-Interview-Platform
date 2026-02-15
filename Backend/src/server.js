e744431e39eab2bf2af92197454bd1e6433ba384
kanhaiya kumar <kk7077574@gmail.com>
2026-02-15 13:06:59 +0530

    Add backend and frontend integration with Clerk authentication and MongoDB connection

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import {ENV} from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 const app = express();
 
 app.get("/HEALTH",(req,res)=>{
@@ -8,4 +9,13 @@ app.get("/HEALTH",(req,res)=>{
 console.log(ENV.DB_URL);
 
 
-app.listen(ENV.PORT, ()=> console.log("Server is Running on Port: ",ENV.PORT)); 
+const startServer = async () => {
+    try{
+        await connectDB();
+        app.listen(ENV.PORT,()=> console.log(`✅ Server is running on port ${ENV.PORT}`));
+    }catch(error){
+        console.log("❌ Error starting server:",error);
+        process.exit(1);
+    }
+};
+startServer();
