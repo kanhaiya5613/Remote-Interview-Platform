b888c1047cd033e7e017d50d7967d5e53530e50f
kanhaiya kumar <kk7077574@gmail.com>
2026-02-20 01:12:42 +0530

    database connect

--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 import {ENV} from "./env.js";
 
 export const connectDB = async () => {
+    if(!ENV.DB_URL){
+        console.log("❌ DB_URL is not defined in environment variables");
+        process.exit(1);
+    }
     try{
         const conn = await mongoose.connect(ENV.DB_URL);
         console.log("✅ Connected to MongoDB:",conn.connection.host);
