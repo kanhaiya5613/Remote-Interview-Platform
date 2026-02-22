3e8a34a9c830a9ec126641bce89c6ec54bdacd07
kanhaiya kumar <kk7077574@gmail.com>
2026-02-23 00:41:10 +0530

    Error while deleting

--- Backend/src/lib/stream.js
+++ Backend/src/lib/stream.js
@@ -1,31 +1,37 @@
-import {StreamChat} from "stream-chat";
+import { StreamChat } from "stream-chat";
 import { ENV } from "./env.js";
 
 const apiKey = ENV.STREAM_API_KEY;
 const apiSecret = ENV.STREAM_API_SECRET;
 
-if(!apiKey || !apiSecret){
-    console.error("Stream API key or secret is missing. Please check your environment variables.");
+if (!apiKey || !apiSecret) {
+  console.error("Stream API key or secret missing");
 }
 
-export const chatClient = new StreamChat.getInstance(apiKey, apiSecret);
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
 
-export const upsertStreamUser = async(userData)=>{
-    try {
-        await chatClient.upsertUser(userData)
-        console.log("Stream user Upserted Successfully:" , userData)
-    } catch (error) {
-        console.error("Error while upserting stream user: ", error);
-    }
-}
+export const upsertStreamUser = async (userData) => {
+  try {
+    await chatClient.upsertUser(userData);
+    console.log("Stream user upserted:", userData);
+  } catch (error) {
+    console.error("Upsert error:", error);
+  }
+};
 
-export const deleteStreamUser = async(userId)=>{
-    try {
-        await chatClient.upsertUser(userId)
-        console.log("Stream User Deleted Successfully:", userId);
-        
-    } catch (error) {
-        console.error("Error while deleting stream user: ", error);
-    }
-}
-// todo: add another method to generateToken 
+export const deleteStreamUser = async (userId) => {
+  try {
+    await chatClient.deleteUser(userId, { hard_delete: true });
+    console.log("Stream user deleted:", userId);
+  } catch (error) {
+    console.error("Delete error:", error);
+  }
+};
+
+export const generateStreamToken = (userId) => {
+  try {
+    return chatClient.createToken(userId);
+  } catch (error) {
+    console.error("Token error:", error);
+  }
+};
