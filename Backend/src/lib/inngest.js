ce1bb8f44017199fa51a1ae408736c414ffc53b1
kanhaiya kumar <kk7077574@gmail.com>
2026-02-23 00:14:00 +0530

    Auth Stream Sync

--- Backend/src/lib/inngest.js
+++ Backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import {Inngest} from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 
 export const inngest = new Inngest({id:"Remote Interview Platform"});
@@ -22,7 +23,11 @@ const syncUser = inngest.createFunction(
             profileImage: image_url || "",
         }
         await User.create(newUser);
-        // todo : do sth else
+        await upsertStreamUser({
+          id: newUser.clerkId.toString(),
+          name: newUser.name,
+          image: newUser.profileImage
+        })
     }
 )
 
@@ -34,7 +39,7 @@ const deleteUserFromDB = inngest.createFunction(
 
         const {id}= event.data;
         await User.deleteOne({clerkId});
-        // todo : do sth else
+        await deleteStreamUser(id.toString());
     }
 )
 export const functions = [syncUser, deleteUserFromDB]
