05ceb9b52232ece9d9dd66024ca0b63cc7434f78
kanhaiya kumar <kk7077574@gmail.com>
2026-02-24 15:11:28 +0530

    Add session management features with video call and chat integration

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,6 +7,7 @@ import { inngest, functions } from "./lib/inngest.js";
 import path from "path";
 import { clerkMiddleware } from '@clerk/express'
 import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoutes.js";
 const app = express();
 const __dirname = path.resolve();
 app.use(express.json());
@@ -15,6 +16,7 @@ app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 app.use(clerkMiddleware())
 app.use("/api/inngest",serve({ client: inngest, functions}));
 app.use("/api/chat",chatRoutes);
+app.use("/api/sessions",sessionRoutes);
 app.get("/HEALTH",(req,res)=>{
     res.status(200).json({msg:"api is up and running"})
 });
