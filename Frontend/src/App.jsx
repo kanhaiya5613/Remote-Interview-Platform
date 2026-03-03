8e1cd6c99c49efa8ccfbb21b20347e9c6a6a1259
kanhaiya kumar <kk7077574@gmail.com>
2026-03-03 23:48:56 +0530

    Landing page Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,18 @@ import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser }
 import { Navigate, Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage';
 import ProblemsPage from './pages/ProblemsPage';
+import DashboardPage from './pages/DashboardPage';
 import { Toaster } from "react-hot-toast";
 function App() {
-  const {isSignedIn} = useUser();
+  const {isSignedIn,isLoaded} = useUser();
+  // this will fet rif of flickring effect
+  if(!isLoaded) return null;
   return (
     <>
     <Toaster toastOptions={{duration:3000}}/>
    <Routes>
-    <Route path="/" element={<HomePage/>}/>
+    <Route path="/" element={!isSignedIn?<HomePage/>:<Navigate to={"/dashboard"}/>}/>
+    <Route path="/dashboard" element={isSignedIn?<DashboardPage/>:<Navigate to={"/"}/>}/>
     <Route path="/problems" element={ isSignedIn ? <ProblemsPage/> : <Navigate to={"/"}/>}/>
    </Routes>
    </>
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,22 +1,155 @@
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react'
-import React from 'react'
-import toast from 'react-hot-toast'
-
+import { Link } from "react-router"
+import { CheckIcon, Target, UsersIcon, VideoIcon, Code2Icon,ZapIcon, UserIcon } from "lucide-react"
+import { SignInButton } from "@clerk/clerk-react"
+import { ArrowRightIcon } from "lucide-react"
 function HomePage() {
   return (
-    <>
-    <button className='btn btn-primary' onClick={()=> toast.success("Successfully Clicked")}>Click me</button>
-    <SignedOut>
-      <SignInButton mode='model'>
-        <button>Login</button>
-      </SignInButton>
-    </SignedOut>
-    <SignedIn>
-        <SignOutButton/>
-    </SignedIn>
+    <div className="bg-gradient-to-br from-base-100 via-base-200 to-base-300">
+      {/* #NAVBAR */}
+      <nav className="bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-lg">
+        <div className="ma-auto p-4 flex items-center justify-between">
+          {/* LOGO */}
+          <Link to={"/"}
+            className="flex items-center gap-3 hover:scale-105 transition-transform duration-200">
+            <div>
+              <div className="size-10 rounded-xl bg-gradient-to-br from-primary via-secondary to-accent flex items-center justify-center shadow-lg">
+                <Target className="sixe-6 text-white" />
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <span className="font-black text-xl bg-gradient-to-r from-primary via-secondary to-accent bg-clip-text text-transparent font-mono tracking-wider">
+                HireCandy
+              </span>
+              <span className="text-xs text-base-content/60 font-medium -mt-1">Hire Talent</span>
+            </div>
+          </Link>
+          {/* AUTHENTICATION */}
+          <SignInButton mode="modal">
+            <button className="group px-6 py-3 bg-gradient-to-r from-primary to-secondary rounded-xl text-white font-semibold text-sm shadow-lg hover:shadow-xl transition-all duration-200 hover:scale-105 flex items-center gap-2">
+              <span>Get Started</span>
+              <ArrowRightIcon className="size-4 group-hover:translate-x-0.5 translate-transform" />
+            </button>
+          </SignInButton>
+        </div>
 
-    <UserButton/>
-    </>
+      </nav>
+      {/* HERO SECTION */}
+      <div className="max-w-7xl mx-auto px-4 py-20">
+        <div className="grid lg:grid-cols-2 gap-12 items-center">
+          {/* LEFT CONTENT */}
+          <div className="space-y-8">
+            <div className="badge badge-primary badge-lg">
+              <ZapIcon className="size-4" />
+              Real-Time Collaboration
+            </div>
+            <h1 className="text-5xl lg-text-7xl font-black leading-tight">
+              <span className="bg-gradient-to-r from-primary via-secondary to-accent bg-clip-text text-transparent">
+                Test Candidate,
+              </span>
+              <br />
+              <span className="text-base-content">Hire Talent</span>
+            </h1>
+            <p className="text-xl text-base-content/70 leading-relexed max-w-xl text-mono">
+              Conduct seamless, real-time technical interviews from anywhere. Evaluate candidates with live coding, video calls, and smart assessment tools — all in one secure platform.
+            </p>
+            {/* FEATURES */}
+            <div className="flex flex-wrap gap-3">
+              <div className="badge badge-lg badge-outline border rounded-2xl">
+                <CheckIcon className="size-4 text-success"/>
+                Live Video Chat
+              </div>
+              <div className="badge badge-lg badge-outline border rounded-2xl">
+                <CheckIcon className="size-4 text-success"/>
+                Code Editor
+              </div>
+              <div className="badge badge-lg badge-outline border rounded-2xl">
+                <CheckIcon className="size-4 text-success"/>
+                Multi-Language
+              </div>
+            </div>
+            {/* CTA */}
+            <div className="flex flex-wrap gap-4">
+              <SignInButton mode="modal">
+                <button className="btn  btn-primary btn-lg rounded-3xl">
+                  Start Coding Now
+                  <ArrowRightIcon className="size-5"/>
+                </button>
+              </SignInButton>
+              <button className="btn btn-outline btn-lg rounded-3xl">
+                <VideoIcon className="size-5"/>
+                Watch Demo
+              </button>
+            </div>
+
+            {/* STATS */}
+            <div className="stats stats-vertical lg:stats-horizontal bg-base-100 shadow-lg">
+              <div className="stat">
+                <div className="stat-value text-primary">10K+</div>
+                <div className="stat-title">Active Users</div>
+              </div>
+              <div className="stat">
+                <div className="stat-value text-secondary">50K+</div>
+                <div className="stat-title">Sessions</div>
+              </div>
+              <div className="stat">
+                <div className="stat-value text-accent">99.9%</div>
+                <div className="stat-title">Uptime</div>
+              </div>
+            </div>
+          </div>
+          {/* RIGHT CONTENT */}
+          <img 
+          src="/hero.png"
+          alt="CodeCollab Platform"
+          className="w-full h-auto rounded-3xl shadow-2xl border-4 border-base-100 hover:scale-105 transition-transform duration-500"
+          />
+        </div>
+
+      </div>
+      {/* FEATURES */}
+      <div className="max-w-7xl mx-auto px-4 py-20">
+        <div className="text-center mb-16">
+          <h2 className="text-4xl font-bold mb-4">
+            Everything You Need to <span className="text-primary font-mono"> Succeed</span>
+          </h2>
+          <p className="text-lg text-base-content/70 max-w-2xl mx-auto">
+          Powerful features designed to make your coding interviews seamless and productive</p>
+        </div>
+        {/* FEATURE GRID */}
+        <div className="grid md:grid-cols-3 gap-8">
+          {/* FEATURE 1 */}
+          <div className="card bg-base-100 shadow-xl">
+            <div className="card-body items-center text-center">
+              <div className="size-16 bg-primary/10 rounded-2xl flex items-center justify-center mb-4">
+              <VideoIcon className="size-8 text-primary"/>
+             </div>
+             <h3 className="card-title"> HD Video Call</h3>
+             <p className="text-base-content">Crystal clear video and audio for seamless communication during interviews</p>
+            </div>
+          </div>
+          {/* FEATURE 2 */}
+          <div className="card bg-base-100 shadow-xl">
+            <div className="card-body items-center text-center">
+              <div className="size-16 bg-primary/10 rounded-2xl flex items-center justify-center mb-4">
+              <Code2Icon className="size-8 text-primary"/>
+             </div>
+             <h3 className="card-title">Live Code Editor</h3>
+             <p className="text-base-content">Collaborate in real-time with syntex highlighting and multiple language support</p>
+            </div>
+          </div>
+          {/* Feature 3 */}
+          <div className="card bg-base-100 shadow-xl">
+            <div className="card-body items-center text-center">
+              <div className="size-16 bg-primary/10 rounded-2xl flex items-center justify-center mb-4">
+              <UsersIcon className="size-8 text-primary"/>
+             </div>
+             <h3 className="card-title"> Easy Collaboration</h3>
+             <p className="text-base-content">Share your Screen, discuss solutions, and learn from each other in real time</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
