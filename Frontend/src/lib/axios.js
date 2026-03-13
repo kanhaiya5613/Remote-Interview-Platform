d8d140b63086d253c3fb94644854df0847c135b0
kanhaiya kumar <kk7077574@gmail.com>
2026-03-14 02:42:45 +0530

    fix: remove console log for API_BASE in axios configuration

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -3,8 +3,6 @@ import axios from "axios";
 const API_BASE =
   import.meta.env.VITE_API_URL || "https://remote-interview-platform-2.onrender.com/api";
 
-console.log("API BASE:", API_BASE);
-
 const axiosInstance = axios.create({
   baseURL: API_BASE,
   withCredentials: true,
