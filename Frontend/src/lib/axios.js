eadf3b802d1649ad361ad32c80a2f12aa281dfc5
kanhaiya kumar <kk7077574@gmail.com>
2026-03-13 14:57:50 +0530

    fix: add console log for VITE_API_URL in axios and session API files for debugging

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
-
+console.log(import.meta.env.VITE_API_URL)
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
+    
     withCredentials: true, // By adding this field browser will send the cookies to server automatically, on every single request
 });
 
